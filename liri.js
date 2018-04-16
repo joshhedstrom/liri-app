6baaa61397e2b6f8d39e175d3675a44debc32a5f
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-04-16 18:10:06 -0500

    added switch case statements

--- liri.js
+++ liri.js
@@ -1,3 +1,40 @@
-require("dotenv").config();
+require("./.env").config();
+const request = require('request');
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
+
+let action = process.argv[2];
+let item = process.argv[3];
+
+switch (action) {
+    case 'my-tweets':
+    let query = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=josh_hedstrom&count=2"
+        request(query, (error, response, body)=> {
+        	// if(err) return console.log(err);
+
+        	console.log('twitter: ' + query);
+        	console.log(body)
+
+        })
+        break;
+    case 'spotify-this-song':
+        // statements_1
+        break;
+    case 'movie-this':
+        // statements_1
+        break;
+    case 'do-what-it-says':
+        // statements_1
+        break;
+    default:
+        // statements_def
+        break;
+}
+
+// * `my-tweets`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
