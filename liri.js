1f4a1f97c12a11805c5b16498823449e62ddee03
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-04-19 09:14:29 -0500

    completed omdb functionality

--- liri.js
+++ liri.js
@@ -20,9 +20,9 @@ switch (action) {
 
         }
         client.get('statuses/user_timeline', tweetProps, (err, body, res) => {
-            if (err) return console.log(err);
+            if (err) return console.log('ERROR: ', err);
             body.forEach((elem, i) => {
-                console.log('---->>Tweet #', i + 1, '--------------------', 'on: ', elem.created_at);
+                console.log('---->>Tweet #', i + 1, '---------------------------------------', 'on: ', elem.created_at);
                 console.log('');
                 console.log('---->>', elem.text);
                 console.log('');
@@ -33,14 +33,19 @@ switch (action) {
         break;
 
     case 'spotify-this-song':
-        spotify.search({type: 'track', query: item, limit: 5, from: 'us'}, (err, data) => {
-            if (err) return console.log(err);
+        spotify.search({
+            type: 'track',
+            query: item,
+            limit: 5,
+            from: 'us'
+        }, (err, data) => {
+            if (err) return console.log('ERROR: ', err);
             let result = data.tracks.items;
             result.forEach((elem, i) => {
-                console.log('---->>Song #', i + 1, '--------------------');
+                console.log('---->>Song #', i + 1, '-----------------------------------------------------------------');
                 console.log('');
                 console.log('Artist:-->>', elem.artists[0].name);
-                console.log('Name:---->>', elem.name);   
+                console.log('Name:---->>', elem.name);
                 console.log('Preview:->>', elem.preview_url);
                 console.log('Album:--->>', elem.album.name);
                 console.log('');
@@ -52,7 +57,25 @@ switch (action) {
         break;
 
     case 'movie-this':
-        // statements_1
+    let url = "http://www.omdbapi.com/?t=" + item + "&y=&plot=short&apikey=trilogy";
+        request(url , (err, res, body) => {
+            if (err) return console.log('ERROR: ', err);
+            let movie = JSON.parse(body);
+            console.log('Result------->>--------------------------------------------------------------------------');
+            console.log('');
+            console.log('Title:------->>', movie.Title);
+            console.log('Year:-------->>', movie.Year);
+            console.log('IMDB Rating:->>', movie.Ratings[0].Value);
+            console.log('RT Rating:--->>', movie.Ratings[1].Value);
+            console.log('Country:----->>', movie.Country);
+            console.log('Language:---->>', movie.Language);
+            console.log('Actors:------>>', movie.Actors);
+            console.log('Plot:-------->>', movie.Plot);
+            console.log('');
+            console.log('-----------------------------------------------------------------------------------------');
+            console.log('');
+        });
+
         break;
 
     case 'do-what-it-says':
